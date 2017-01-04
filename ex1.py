f25bff94e90b286b338911071a6490ddd1cbe8eb
ardaerturk <arda.erturk@mail.utoronto.ca>
2017-01-04 15:21:27 -0500

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,11 @@
+def useless(a, b, c):
+    return "That was a waste of time"
+
+
+def square_me(input_number):
+    result = input_number ** 2
+    return result
+
+def student_data(name,age,student_number,enrolled):
+    result = '<%s,%s,%s,%s>' % (student_number,name,age,enrolled)
+    return result
